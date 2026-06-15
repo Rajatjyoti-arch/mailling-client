a1245e6c19af84486df1210e8907e1ebd7e31727
Rajatjyoti-arch <rajatb.0307@gmail.com>
2026-06-15 17:30:29 +0530

    Worked on main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import smtplib
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
+
 server.ehlo()
-server.starttls() 
-server.ehlo()
+
+with open("accounts.txt", "r") as f:
+    email = f.readline().strip()
+    password = f.readline().strip()
+
+server.login(email, password)
